210e5c0bb7644d7a3cc191fa14737a7c157002f0
schollmeyer <georg.schollmeyer@stat.uni-muenchen.de>
2022-10-20 21:20:37 +0200

    testfkt tukeys outlyingness erstellt

--- tests/testthat/test-tukeys_depth.R
+++ tests/testthat/test-tukeys_depth.R
@@ -1,8 +1,21 @@
 test_that("compute_tukeys_outlyingness works", {
   context <- diag(rep(1, 10))
+  outlyingness <- compute_tukeys_outlyingness(context[1,], context)
+  expect_equal(outlyingness, 0.1)
+})
+
+
+test_that("compute_tukeys_outlyingness works", {
+  context <- diag(rep(1, 10))
   outlyingness <- compute_tukeys_outlyingness(context, context)
   names(outlyingness) <- NULL
   expect_equal(outlyingness, rep(0.1, 10))
+})
+
+test_that("compute_tukeys_depth works", {
+  context <- diag(rep(1, 10))
+  depth <- compute_tukeys_depth(context[1,], context)
+  expect_equal(depth, 0.9)
 })
 
 test_that("compute_tukeys_depth works", {
