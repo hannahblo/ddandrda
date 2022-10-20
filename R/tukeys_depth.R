7a5fbce48062a1d3b65163a2dac16366d796bdb9
schollmeyer <georg.schollmeyer@stat.uni-muenchen.de>
2022-10-20 15:16:27 +0200

    tukeys outlyingndess definiert

--- R/tukeys_depth.R
+++ R/tukeys_depth.R
@@ -1,13 +1,15 @@
-compute_tukeys_outlyingness <- function(context,weights=rep(1,nrow(context))){  
+compute_tukeys_outlyingness <- function(context, weights = rep(1, nrow(context))) {
   n <- nrow(context)
   m <- ncol(context)
-  A <- rep(0,m)
-  for(k in (1:m)){
-    A[k] <- mean(context[,k]*weights)}
-  l <- rep(0,n)
-  
-  for(k in (1:n)){
-    temp <- context[k,]
-    l[k] <- max(A[which(temp==0)])
+  A <- rep(0, m)
+  for (k in (1:m)) {
+    A[k] <- mean(context[, k] * weights)
   }
-return(l)}
+  l <- rep(0, n)
+
+  for (k in (1:n)) {
+    temp <- context[k, ]
+    l[k] <- max(A[which(temp == 0)])
+  }
+  return(l)
+}
