af81344fd7fb3bf5376bdd5654c0e3c669aeacbd
schollmeyer <georg.schollmeyer@stat.uni-muenchen.de>
2022-10-20 17:56:33 +0200

    Tukeys_depth.R geadded

--- R/tukeys_depth.R
+++ R/tukeys_depth.R
@@ -1,7 +1,7 @@
-compute_outlyingness <- function(intent,
-                                 context,
-                                 row_weights = rep(1, nrow(context)),
-                                 col_weights = rep(1, ncol(context))) {
+compute_tukeys_outlyingness <- function(intent,
+                                        context,
+                                        row_weights = rep(1, nrow(context)),
+                                        col_weights = rep(1, ncol(context))) {
   normed_row_weights <- row_weights / sum(row_weights)
   weighted_context <- ((normed_row_weights) %*% t(col_weights)) * context
   weighted_column_means <- t(weighted_context) %*% (rep(1, nrow(context)))
