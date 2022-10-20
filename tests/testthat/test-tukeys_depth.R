638a5254e65b9f8446bbc0c0470c43b9bdab92ed
schollmeyer <georg.schollmeyer@stat.uni-muenchen.de>
2022-10-20 20:49:22 +0200

    testfkt tukeys outlyingness erstellt

--- tests/testthat/test-tukeys_depth.R
+++ tests/testthat/test-tukeys_depth.R
@@ -1,3 +1,5 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("compute_tukeys_outlyingness works", {
+  context <- diag(rep(1,10))
+  outlyingness <- compute_tukeys_outlyingness(context,context)
+  expect_equal(outlyingness, rep(0.1,10))
 })
