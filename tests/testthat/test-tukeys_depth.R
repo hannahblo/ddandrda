f90b04efe9b2ab69524206b3bd7c9cf1a02b71d2
schollmeyer <georg.schollmeyer@stat.uni-muenchen.de>
2022-10-21 09:42:15 +0200

    m

--- tests/testthat/test-tukeys_depth.R
+++ tests/testthat/test-tukeys_depth.R
@@ -24,3 +24,29 @@ test_that("compute_tukeys_depth works", {
   names(depth) <- NULL
   expect_equal(depth, rep(0.9, 10))
 })
+
+
+test_thate("compute_tukeys_median_order works", {
+  q <- sample((3:5),size=1)
+  a <- compute_all_partial_orders(q,complemented=FALSE)
+  orders <- list()
+  m <- nrow(a)
+  for(k in (1:m)){temp <- a[k,];dim(temp) <- c(q,q);orders[[k]] <- cbind(temp,1-temp)}
+  i <- sample((1:m),size=ceiling(nrow(a)/3))
+  a <- cbind(a,1-a)
+  ans1 <- compute_tukeys_median_order(orders[i])
+  td <- compute_tukeys_depth(rbind(a[i,],a),weights=c(rep(1,length(i)),rep(0,m)))
+  j <- which(td==max(td))
+
+  output=FALSE
+  for(jj in j){
+    ans2 <- (rbind(a[i,],a))[jj,];dim(ans2) <- c(q,2*q)
+        if(all(ans1==ans2)){output=TRUE}
+  }
+
+  expect_equal(output,TRUE)
+
+
+
+
+})
