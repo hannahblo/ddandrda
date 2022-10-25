3bd5e2da924e4449e442f009529f24df4de64078
schollmeyer <georg.schollmeyer@stat.uni-muenchen.de>
2022-10-25 19:40:53 +0200

    neuse file optim_fca angelegt

---
+++ R/optim_fca.R
@@ -0,0 +1,92 @@
+compute_context_all_porders
+
+
+
+return_eins <- function(...) {
+  1
+}
+
+
+
+sample_concept <- function(context, steps = 10000, f) {
+  maximum <- -Inf
+  m <- dim(context)[1]
+  n <- dim(context)[2]
+  top_attr <- operator_closure_attr_input(rep(0, n), bg)
+  bottom_obj <- operator_closure_obj_input(rep(0, m), bg)
+
+  if (runif(1) > 0.5) {
+    a <- rep(1, m)
+    b <- top.attr
+  } else {
+    b <- rep(1, n)
+    a <- bottom_obj
+  }
+
+  for (i in (1:steps)) {
+    if (runif(1) > 0.5) {
+      l <- sample((1:n), size = 1)
+      bb <- b
+      bb[l] <- 1
+      aa <- calculate_phi(bb, context)
+      bb <- calculate_psi(aa, context)
+      alpha <- g_ext(aa, a, context) * n / g_int(b, bb, context) / m
+      if (is.na(alpha)) {
+        alpha <- 0
+      }
+    } else {
+      o <- sample((1:m), size = 1)
+      aa <- a
+      aa[o] <- 1
+      bb <- calculate_psi(aa, context)
+      aa <- calculate_phi(bb, context)
+      alpha <- g_int(bb, b, context) * m / ge_ext(a, aa, context) / n
+      if (is.na(alpha)) {
+        alpha <- 0
+      }
+    }
+    #
+    if (f(b, context) > maximum) {
+      b_maximum <- b
+    }
+    maximum <- max(maximum, f(b, context))
+    if (runif(1) < alpha * f(bb, context) / f(b, context)) {
+      a <- aa
+      b <- bb
+    }
+    # Diesen Teil brauchen wir nichtif (runif(1) >= Q) {
+    # b <- b_maximum
+    # a <- calculate_phi(b, context)
+    # }
+  }
+
+
+  print(c("alpha", alpha))
+  return(b)
+}
+# T=concept.gen(bg,15000,f)
+
+g_int <- function(b, bb, context) {
+  if (all(b == bb)) {
+    return(0)
+  }
+  ans <- 0
+  i <- which(bb == 1 & b == 0)
+  for (k in i) {
+    temp <- b
+    temp[k] <- 1
+    if (all(operator_closure_attr_input(temp, context) == bb)) {
+      ans <- ans + 1
+    }
+  }
+  return(ans)
+}
+
+
+
+g_ext <- function(a, aa, context) {
+  g_int(a, aa, t(bg$context))
+}
+
+
+# T=concept.gen(bg,10000,f)
