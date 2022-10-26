7bbd9ecff8e806ad16ec4934a95f494f5d12f44c
schollmeyer <georg.schollmeyer@stat.uni-muenchen.de>
2022-10-26 02:42:41 +0200

    tukeys depth und optim fca gereinigt

--- R/optim_fca.R
+++ R/optim_fca.R
@@ -13,23 +13,88 @@
 #' #'
 #' @param q is the number of elements of the basic space
 #' @param names are the names of the q elements
+#'
+#' @examples
+#' q <- 5
+#' steps <- 10000
+#' context_for_q_porders <- compute_context_all_porders(q=q)
+#' corders <- compute_all_partial_orders(q=q,complemented=TRUE,list=TRUE)
+#' context <- list_to_context(corders,complemented=TRUE)
+#' index <- sample((1:nrow(context)),size=3)
+#' sampled_context <- context[index,]
+#' g <- function(intent,context){0.00001+compute_tukeys_depth(c(intent,1-intent)
+#' , sampled_context)}
+#' tukeys_true_median <- compute_tukeys_median_order(corders[index])
+#' tukeys_median_based_on_mc_heuristic <- sample_concept(
+#' context_for_q_porders,steps=steps,g)
+#' par(mfrow=c(2,1))
+#' plot_order(tukeys_true_median$median)
+#' dim(tukeys_median_based_on_mc_heuristic) <- c(q,q)
+#' plot_order(tukeys_median_based_on_mc_heuristic)
+#'
 #' @export
-compute_context_all_porders <- function(q, names=(1:q)) {
+compute_context_all_porders <- function(q, names = (1:q)) {
   perms <- gtools::permutations(q, q)
- colnames(perms) <- names
- context <- ranking_scaling(perms,
-                            remove_full_columns = FALSE,
-                            complemented = FALSE
- )
+  colnames(perms) <- names
+  context <- ranking_scaling(perms,
+    remove_full_columns = FALSE,
+    complemented = FALSE
+  )
 
-return(context)}
+  return(context)
+}
 
-return_eins <- function(...) {
+#' Return 1
+#'
+#' @description A function that always returns 1
+#'
+#' @export
+return_eins <- function() {
   1
 }
 
 
 
+#'  Sampling a formal concept
+#'
+#' 'sample_concept' samples a formal concept of a given formal context
+#' according to a probability function f that assigns to every intent a
+#' corrseponding sampling probability. The algorithm implemented is the one
+#' given based in:
+#'
+#' Mario Boley, Thomas Gärtner and Henrik Grosskreutz:
+#' Formal Concept Sampling for Counting and Threshold-Free Local Pattern Mining.
+#' Proceedings of the 2010 SIAM International Conference on Data Mining (SDM)
+#' pp.177 - 188
+#'
+#' @param context is the underlying context
+#' @param steps is the number of steps within the Markov chain Monte Carlo
+#'  algorithm
+#' @param f is the depth function which assigns to every intent a sampling
+#'  probability. This functions has to have two arguments: the intent and the
+#'  underlying context
+#'
+#' @examples q <- 5
+#' steps <- 10000
+#' context_for_q_porders <- compute_context_all_porders(q = q)
+#' corders <- compute_all_partial_orders(q = q, complemented = TRUE, list = TRUE)
+#' context <- list_to_context(corders, complemented = TRUE)
+#' index <- sample((1:nrow(context)), size = 3)
+#' sampled_context <- context[index, ]
+#' g <- function(intent, context) {
+#'   0.00001 + compute_tukeys_depth(c(intent, 1 - intent), sampled_context)
+#' }
+#' tukeys_true_median <- compute_tukeys_median_order(corders[index])
+#' tukeys_median_based_on_mc_heuristic <- sample_concept(
+#'   context_for_q_porders,
+#'   steps = steps, g
+#' )
+#' par(mfrow = c(2, 1))
+#' plot_order(tukeys_true_median$median)
+#' dim(tukeys_median_based_on_mc_heuristic) <- c(q, q)
+#' plot_order(tukeys_median_based_on_mc_heuristic)
+#'
+#' @export
 sample_concept <- function(context, steps = 10000, f) {
   maximum <- -Inf
   m <- dim(context)[1]
@@ -72,6 +137,8 @@ sample_concept <- function(context, steps = 10000, f) {
       b_maximum <- b
     }
     maximum <- max(maximum, f(b, context))
+
+
 
     if (stats::runif(1) < alpha * f(bb, context) / f(b, context)) {
       a <- aa
